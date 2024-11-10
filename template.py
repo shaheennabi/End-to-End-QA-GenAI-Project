63ae6686d9bcce4db9e5f0f0b4145673ad69774b
shaheennabi <ishaheennabi333@gmail.com>
2024-11-10 10:13:04 -0800

    updated

---
+++ template.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+
+project_name = "us_visa"
+
+list_of_files = [
+
+    f"{project_name}/__init__.py",
+   
+
+]
+
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+    else:
+        print(f"file is already present at: {filepath}")
